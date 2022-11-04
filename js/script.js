e82d7fa39ac7b048e753557069f6fc3305639b78
YogeshTiwari10 <117347105+YogeshTiwari10@users.noreply.github.com>
2022-11-04 12:54:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // constants and variables
 let direction={x: 0 , y:0};
-let foodSound=new Audio('music/food.mp3');
-let gameOverSound= new Audio('music/gameover.mp3');
+let foodSound=new Audio('../../music/food.mp3');
+let gameOverSound= new Audio('../music/gameover.mp3');
 let moveSound= new Audio('music/move.mp3');
 let musicSound=new Audio('music/music.mp3');
 let lastPaintTime=0;
